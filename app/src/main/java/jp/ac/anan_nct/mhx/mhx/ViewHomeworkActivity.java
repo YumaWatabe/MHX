b3c874ff2959b9b7c1916a628b681d5c179ba263
YumaWatabe <1157591@st.anan-nct.ac.jp>
2018-11-30 09:48:32 +0900

    [Add] Added ViewHomeworkActivity

---
+++ app/src/main/java/jp/ac/anan_nct/mhx/mhx/ViewHomeworkActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.anan_nct.mhx.mhx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewHomeworkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_homework);
+    }
+}
